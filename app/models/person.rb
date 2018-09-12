582a949856dbb14bb1d7b38f6b31d229558f3921
Samanthi Jinadasa <sjinadasa@fas.harvard.edu>
2018-09-12 00:12:31 +0000

    Fixed typo in person.rb

--- app/models/person.rb
+++ app/models/person.rb
@@ -4,6 +4,6 @@ class Person < ApplicationRecord
     validates_length_of :secret, in: 6..24
     validates_format_of :secret, with: /[0-9]/, message: "must contain at least one number"
     validates_format_of :secret, with: /[A-Z]/, message: "must contain at least one upper case character"
-    validates_format_of :secret, with: /[a-z];, message: "must contain at least one lower case character"
+    validates_format_of :secret, with: /[a-z]/, message: "must contain at least one lower case character"
     
 end
